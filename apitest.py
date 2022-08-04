23f8a3524a9a11c2810d62737f633c4fe64cd656
storrer <storrer.chris@gmail.com>
2022-08-03 22:44:25 -0700

    Add sample parameters

--- apitest.py
+++ apitest.py
@@ -5,4 +5,24 @@ import requests
 creds = f"{API_APPLICATION_ID}:{API_SECRET_KEY}"
 credsbytes = creds.encode('utf-8')
 credsencode = base64.b64encode(credsbytes)
-header = f'Authorization: Basic {credsencode}'
+header = {'Authorization': f'Basic {credsencode}'}
+endpoint = 'https://api.astronomyapi.com/api/v2/studio/star-chart'
+parameters = {
+    "observer": {
+        "latitude": 33.775867,
+        "longitude": -84.39733,
+        "date": "2019-12-20"
+    },
+    "view": {
+        "type": "area",
+        "parameters": {
+            "position": {
+                "equatorial": {
+                    "rightAscension": 14.83,
+                    "declination": -15.23
+                }
+            },
+            "zoom": 3
+        }
+    }
+}
